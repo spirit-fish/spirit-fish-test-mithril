2d55d3b0399a86a8d6d586bec3e3afdf97d92697
Alicia Willett <alicia@sanctuary.computer>
2019-11-15 13:52:56 -0500

    Load gif

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import m from "mithril";
 
 import HelloWorld from './components/HelloWorld';
+import Giphy from './components/Giphy';
 import NotFound from './components/NotFound';
 const root = document.getElementById("app");
 // m.render(root, NotFound);
@@ -8,7 +9,7 @@ const root = document.getElementById("app");
 m.route(root, "/not-found", {
   "/": {
     render: function () {
-      return m(HelloWorld);
+      return m(Giphy);
     }
   },
   "/not-found": {
