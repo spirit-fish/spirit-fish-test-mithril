db739b32d0bd476111a637aafa27d1bc32984310
Alicia Willett <alicia@sanctuary.computer>
2019-11-15 14:37:46 -0500

    fix routing

--- src/components/NotFound.js
+++
@@ -1,7 +0,0 @@
-import m from "mithril";
-
-const NotFound = m(".not-found", [
-  m('p', 'This page was not found')
-])
-
-export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
 import m from "mithril";
 
-import Giphy from './components/Giphy';
-import NotFound from './components/NotFound';
 const root = document.getElementById("app");
 
-m.route(root, '/', {
-  "/": {
-    render: function () {
-      return m(Giphy);
-    }
+const Giphy = {
+  oninit: function () {
+    this.loadGif();
+    document.title = 'Only The BEST';
   },
-  "/gif": {
-    render: function () {
-      return m(Giphy);
-    }
+  loadGif: function () {
+    m.request({
+      method: "GET",
+      url: 'https://api.giphy.com/v1/gifs/trending?api_key=5DYyxYScARZshsn38FrpiLbFGVFLHcrb&limit=1',
+    }).then(res => {
+      if (res && Array.isArray(res.data) && res.data[0] && res.data[0].url) {
+        this.imgSrc = res.data[0].images.downsized.url;
+      }
+    })
   },
-  '/:any...': {
-    view: NotFound,
+  imgSrc: '',
+  view: function () {
+    return m("#giphy", [
+      m('h1', 'The Best of the Internet, right here:'),
+      ...(
+        this.imgSrc ? [
+          m("img", { src: this.imgSrc, width: '300', height: '300' })
+        ] : [
+          m("p", '...')
+        ]
+      ),
+      m('p', m('a', {
+        href: '#!/not-found'
+      }, 'EXPLORE'))
+    ]);
   }
+}
+
+const NotFound = {
+  view: function() {
+    return m(".not-found", [
+      m('h1', 'Error'),
+      m('p', 'This page was not found'),
+      m('a', {
+        href: '#!/'
+      }, '<- Go back')
+    ])
+  }
+}
+m.route.prefix = '#!';
+m.route(root, '/not-found', {
+  '/': Giphy,
+  '/not-found': NotFound,
+  "/:404...": NotFound
 });
